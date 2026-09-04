4cb42ca7e5a7710fb74c669c4f9ce805572be6ed
Sneha Shree <skatepalli@stud.hs-bremen.de>
2026-09-04 12:11:20 +0200

    removed unwanted files

--- matlab_model/AL58263Q_GateMate_Simulation/step1_single_channel_pwm.m
+++ matlab_model/AL58263Q_GateMate_Simulation/step1_single_channel_pwm.m
@@ -8,7 +8,7 @@ close all;
 
 % AL58263Q has 16-bit brightness control
 MAX_16BIT = 65535;
-pwm_percentage = 80; % Sent by gateMate
+pwm_percentage = 25; % Sent by gateMate
 % Choose one brightness value
 brightness_value = MAX_16BIT * (pwm_percentage/100);   % 50% brightness approximately
 
@@ -36,7 +36,7 @@ fprintf('Average LED current = %.3f mA\n', average_current * 1000);
 
 % Plot PWM current
 figure;
-plot(counter(1:1000), led_current(1:1000) * 1000, 'LineWidth', 1.5);
+plot(counter(1:65535), led_current(1:65535) * 1000, 'LineWidth', 1.5);
 grid on;
 xlabel('PWM counter');
 ylabel('LED current (mA)');
